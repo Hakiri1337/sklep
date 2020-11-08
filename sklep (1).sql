9c433276f6906fcd354fa1787229e87bd4842a86
Hakiri1337 <74145897+Hakiri1337@users.noreply.github.com>
2020-11-08 17:59:55 +0100

    Add files via upload

---
+++ sklep (1).sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 08 Lis 2020, 17:59
+-- Wersja serwera: 10.4.11-MariaDB
+-- Wersja PHP: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `sklep`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `kategoria`
+--
+-- Błąd odczytu struktury tabeli sklep.kategoria: #1932 - Table 'sklep.kategoria' doesn't exist in engine
+-- Błąd odczytu danych dla tabeli sklep.kategoria: #1064 - Something is wrong in your syntax obok 'FROM `sklep`.`kategoria`' w linii 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `klienci`
+--
+-- Błąd odczytu struktury tabeli sklep.klienci: #1932 - Table 'sklep.klienci' doesn't exist in engine
+-- Błąd odczytu danych dla tabeli sklep.klienci: #1064 - Something is wrong in your syntax obok 'FROM `sklep`.`klienci`' w linii 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `logowanie`
+--
+-- Błąd odczytu struktury tabeli sklep.logowanie: #1932 - Table 'sklep.logowanie' doesn't exist in engine
+-- Błąd odczytu danych dla tabeli sklep.logowanie: #1064 - Something is wrong in your syntax obok 'FROM `sklep`.`logowanie`' w linii 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `marka`
+--
+-- Błąd odczytu struktury tabeli sklep.marka: #1932 - Table 'sklep.marka' doesn't exist in engine
+-- Błąd odczytu danych dla tabeli sklep.marka: #1064 - Something is wrong in your syntax obok 'FROM `sklep`.`marka`' w linii 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `pracownik`
+--
+-- Błąd odczytu struktury tabeli sklep.pracownik: #1932 - Table 'sklep.pracownik' doesn't exist in engine
+-- Błąd odczytu danych dla tabeli sklep.pracownik: #1064 - Something is wrong in your syntax obok 'FROM `sklep`.`pracownik`' w linii 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `produkt`
+--
+-- Błąd odczytu struktury tabeli sklep.produkt: #1932 - Table 'sklep.produkt' doesn't exist in engine
+-- Błąd odczytu danych dla tabeli sklep.produkt: #1064 - Something is wrong in your syntax obok 'FROM `sklep`.`produkt`' w linii 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `szczegoly_zamowienia`
+--
+-- Błąd odczytu struktury tabeli sklep.szczegoly_zamowienia: #1932 - Table 'sklep.szczegoly_zamowienia' doesn't exist in engine
+-- Błąd odczytu danych dla tabeli sklep.szczegoly_zamowienia: #1064 - Something is wrong in your syntax obok 'FROM `sklep`.`szczegoly_zamowienia`' w linii 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `zamowienia`
+--
+-- Błąd odczytu struktury tabeli sklep.zamowienia: #1932 - Table 'sklep.zamowienia' doesn't exist in engine
+-- Błąd odczytu danych dla tabeli sklep.zamowienia: #1064 - Something is wrong in your syntax obok 'FROM `sklep`.`zamowienia`' w linii 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
